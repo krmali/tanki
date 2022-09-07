16ab3c10a54fda5f1f60072c6e05cb33ba2bcfa7
Kareem Ali <kareem.ali.rahman@gmail.com>
2022-09-07 13:12:14 +0200

    add keydown event handler

--- src/components/typing.rs
+++ src/components/typing.rs
@@ -1,9 +1,10 @@
 use yew::prelude::*;
 use super::letter::{Letter, Status};
+use gloo::console::log;
 
 #[derive(Properties, PartialEq)]
 pub struct TypingProps{
-    text: String,
+    pub text: String,
 }
 
 impl Default for TypingProps{
@@ -14,8 +15,38 @@ impl Default for TypingProps{
 
 #[function_component(Typing)]
 pub fn typing(TypingProps { text }: &TypingProps) -> Html {
+    let current_index = use_state(|| 0);
+    // let vec : [bool; text.len()] = use_state(|| [0; text.len()]);
+    let vec : UseStateHandle<Vec<bool>> = use_state(|| Vec::with_capacity(text.len()));
+    // let on_key_down = Callback::from(|event: KeyboardEvent| {
+    //     let target = event.target();
+    //     let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
+    //     // let input = target.and_then(|t| t.into());
+    //     // match target{
+    //     //     Some(trgt) => log!(trgt.char_code()),
+    //     //     None => log!("no key down")
+    //     // };
+    //     log!(input.map(|input| input.value()))
+    // });
+    let on_key_down = Callback::from(|event: KeyboardEvent| {
+        log!(event.clone());
+        let input = event.key();
+        if input == "Backspace"{
+            current_index.set(*current_index-1);
+            return;
+        }
+        if input.len() > 1 {return;}
+        if input.bytes().nth(0) == text.bytes().nth(*current_index){
+            current_index.set(*current_index+1);
+        }
+    });
 
-    text.chars().map(|letter| html!(
-            <Letter status={Status::Doing} character={letter}/>
-            )).collect()
+    let letters : Html = text.chars().map(|letter| html!(
+                    <Letter status={Status::Doing} character={letter}/>
+                    )).collect();
+    html!(
+        <div onkeydown={on_key_down} tabindex={0}>
+            {letters}
+        </div>
+        )
 }
