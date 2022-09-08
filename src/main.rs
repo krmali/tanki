62518c21c061610dc3a396603cfdde1660e548f1
Kareem Ali <kareem.ali.rahman@gmail.com>
2022-09-08 08:58:42 +0200

    clean main file

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,7 @@ fn main() {
 
 #[function_component(App)]
 pub fn app() -> Html {
-    // let current_index = use_state(|| 0);
-    // let map = use_state(|| Map<i32, bool>{});
     let text = "The quick brown fox jumps over the lazy dog";
-    // let on_key_down = Callback::from(|event: KeyboardEvent| {
-    //     let target = event.target();
-    //     let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
-    //     // let input = target.and_then(|t| t.into());
-    //     // match target{
-    //     //     Some(trgt) => log!(trgt.char_code()),
-    //     //     None => log!("no key down")
-    //     // };
-    //     log!(input.map(|input| input.value()))
-    // });
-    // let on_key_down = Callback::from(move |event: KeyboardEvent| {
-    //     log!(event.clone());
-    //     let input = event.key();
-    //     if input == "Backspace"{
-    //         current_index.set(*current_index-1);
-    //         return;
-    //     }
-    //     if input.len() > 1 {return;}
-    //     if input.bytes().nth(0) == text.bytes().nth(*current_index){
-    //         current_index.set(*current_index+1);
-    //     }
-    // });
     html!(
         <div>
             <Typing text={text}/>
